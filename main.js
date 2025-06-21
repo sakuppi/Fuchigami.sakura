2236989eba234d222b18e7cee3c7dea4f4f28ae3
sakuppi <f.s-amnos@ezweb.ne.jp>
2025-06-21 12:55:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 1,
     spaceBetween: 30,
-    pagination: {
+    autoHeight: true,    pagination: {
     el: '.swiper-pagination',
     clickable: true,
     },
